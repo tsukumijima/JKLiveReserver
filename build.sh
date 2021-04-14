ea92bf3552790f811b0d3f1657603f3f04f1ef33
tsukumijima <tsukumijima@users.noreply.github.com>
2021-04-14 21:27:30 +0900

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# WSL で作業していて、かつ Windows 側にも Python の環境があり、
+# さらに requirements.txt 内のライブラリが全て入っていることが条件
+
+# Windows 版バイナリをビルド
+pwsh.exe -Command pyinstaller JKLiveReserver.py --onefile --exclude-module numpy --exclude-module pandas
+mv dist/JKLiveReserver.exe JKLiveReserver.exe
+chmod a+x JKLiveReserver.exe
+
+# Linux 版バイナリをビルド
+pyinstaller JKLiveReserver.py --onefile --exclude-module numpy --exclude-module pandas
+mv dist/JKLiveReserver JKLiveReserver
+chmod a+x JKLiveReserver
