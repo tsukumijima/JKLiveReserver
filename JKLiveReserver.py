0db5efef51fc0936edf3d049e088a6d33b4b94ad
tsukumi <tsukumijima@users.noreply.github.com>
2022-03-21 09:29:53 +0900

    Release version 3.5.0

--- JKLiveReserver.py
+++ JKLiveReserver.py
@@ -14,7 +14,7 @@ import time
 from JKLive import JKLive
 
 # バージョン情報
-__version__ = '3.4.0'
+__version__ = '3.5.0'
 
 # このファイルが存在するフォルダの絶対パス
 current_folder = os.path.dirname(os.path.abspath(sys.argv[0]))
